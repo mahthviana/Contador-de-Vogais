02fd69323af510f3bd81798c18a12417692d0968
mahthviana <94473682+mahthviana@users.noreply.github.com>
2024-06-11 12:00:18 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const contarVogais = (str) => {
+  str = str.toLowerCase()
+  const vogais = "aeiou"
+
+  let contagem = 0
+
+  for (let i = 0; i < str.length; i++) {
+    if (vogais.includes(str[i])) {
+      contagem++
+    }
+  }
+  return contagem;
+}
+
+const atualizarResultado = () => {
+  const inputString = document.getElementById('inputString').value
+  alert(inputString)
+  const resultado = contarVogais(inputString)
+
+  document.getElementById('result').innerHTML = `O número de vogais em '${inputString}' é: ${resultado}`
+}
+
+document.getElementById('countButton').addEventListener('click', atualizarResultado)
